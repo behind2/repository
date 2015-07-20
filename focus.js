6a1e0c048e1e56ab1e5636cae2688be36022ba0d
behindli <behindli@tencent.com>
2015-07-20 12:14:48 +0800

    改版后

--- focus.js
+++ focus.js
@@ -30,7 +30,7 @@ define(['jquery'], function ($) {
          * version 版本号
          * @type {String}
          */
-        this.version = '1.0.0';
+        this.version = '1.0.2';
         /**
          * tabId 页卡容器ID
          * @type {String}
@@ -67,6 +67,11 @@ define(['jquery'], function ($) {
          */
         this.defShowTabIdx = options.defShowTabIdx || 0;
         /**
+         * isLoop 是否循环
+         * @type {Boolean}
+         */
+        this.isLoop = options.isLoop || false;
+        /**
          * isAuto 是否自动切换
          * @type {Boolean}
          */
@@ -96,11 +101,32 @@ define(['jquery'], function ($) {
          * @type {String}
          */
         this.nextBtnId = options.nextBtnId || null;
+        /**
+         * switchBefore 切换前
+         * @type {Function}
+         */
+        this.switchBefore = options.switchBefore || $.noop;
+        /**
+         * switchAfter 切换后
+         * @type {Function}
+         */
+        this.switchAfter = options.switchAfter || $.noop;
+        /**
+         * isCheck 是否校验相关参数
+         * @type {Boolean}
+         */
+        this.isCheck = options.isCheck || false;
+        /**
+         * step 滚动步长
+         * @type {Number} units/px
+         */
+        this.step = options.step || null;
 
         // API分层
         this.view = {};//业务暂不需要
         this.model = {};//保护隐藏, 不暴露
         this.ctrl = {};
+
         /*---------------------------------------end---------------------------------------*/
         /**
          * checkTabsNum 校验页签与页卡数量是否一致
@@ -111,53 +137,50 @@ define(['jquery'], function ($) {
         var checkTabsNum = function (tabTag, tabContentTag) {
             return _this.tabContainer.find(tabTag).size() === _this.tabContentContainer.find(tabContentTag).size() ? true : false;
         };
-
-        if ( this.tabId && this.tabTag && this.tabContentId && this.tabContentTag ) {//如果不传或为空抛出异常，提示错误
+        /**
+         * checkOptions 检验初始化参数
+         */
+        var checkOptions = function () {
             /**
              * tabContainer 页卡标签容器
              * @type {Object}
              */
-            this.tabContainer = $('#' + this.tabId);
+            _this.tabContainer = $('#' + _this.tabId);
+            if ( _this.isCheck && _this.tabContainer.size() === 0 ) {//页面页签容器不存在
+                throw new Error('页签容器ID不存在, 请检查.');
+            }
+
             /**
              * tabContentContainer 页卡内容容器
              * @type {Object}
              */
-            this.tabContentContainer = $('#' + this.tabContentId);
-            /*************************************************************华丽的分割线**************************************************************/
-
-            if ( this.tabContainer.size() === 0 ) {//页面页签容器不存在
-                console.log('%c页签容器ID不存在, 请检查.', 'color:red');
+            _this.tabContentContainer = $('#' + _this.tabContentId);
+            if ( _this.isCheck && _this.tabContentContainer.size() === 0 ) {//页卡内容容器不存在
+                throw new Error('页签内容容器ID不存在, 请检查.');
             }
 
-            if ( this.tabContentContainer.size() === 0 ) {//页卡内容容器不存在
-                console.log('%c页签内容容器ID不存在, 请检查.', 'color:red');
+            if ( _this.isCheck && !checkTabsNum(_this.tabTag, _this.tabContentTag) ) {
+                throw new Error('页签与内容数量不一致, 请校验.');
             }
-
-            if (!checkTabsNum(this.tabTag, this.tabContentTag)) {
-                console.log('%c页签与内容数量不一致, 请校验.', 'color:red');
-            } else {
-                /*************************************************************华丽的分割线**************************************************************/
-                // 补充声明
-                /**
-                 * tabTags 页签List
-                 * @type {Array}
-                 */
-                this.tabTags = this.tabContainer.find(this.tabTag);
-                /**
-                 * tabContentTags 页卡内容标签List
-                 * @type {Array}
-                 */
-                this.tabContentTags = this.tabContentContainer.find(this.tabContentTag);
-                /**
-                 * tabSize 页签数量
-                 * @type {Number}
-                 */
-                this.tabSize = this.tabTags.size();
-            }
-
-        } else {
-            console.log('%c缺少必要的参数, 请检查.', 'color:red');
-        }
+            // 补充声明
+            /**
+             * tabTags 页签List
+             * @type {Array}
+             */
+            _this.tabTags = _this.tabContainer.find('>' + _this.tabTag);
+            /**
+             * tabContentTags 页卡内容标签List
+             * @type {Array}
+             */
+            _this.tabContentTags = _this.tabContentContainer.find('>' + _this.tabContentTag);
+            /**
+             * tabSize 页签/内容数量(页签有可能不存在或者隐藏)
+             * @type {Number}
+             */
+            _this.tabSize = _this.tabContentTags.size();
+        };
+        // 调用
+        checkOptions();
 
         /**
          * extraInit 额外初始化
@@ -166,10 +189,16 @@ define(['jquery'], function ($) {
             // 克隆
             var _first = _this.tabContentTags.eq(0).clone(true, true);
             var _last = _this.tabContentTags.eq(_this.tabSize - 1).clone(true, true);
+
+            // 不循环了
+            if (!_this.isLoop) {
+                _first.hide();
+            }
+
             // 插入
             _last.prependTo(_this.tabContentContainer);
             _this.tabContentContainer.append(_first);
-            _this.tabContentContainer.css({width: (_this.tabSize + 2) * parseInt(_this.tabContentTags.eq(0).css('width'))});
+            _this.tabContentContainer.css({width: (_this.tabSize + 2) * parseInt(_this.tabContentTags.eq(0).outerWidth(true))});
         };
         // 当为滚动类型的焦点图时需要额外初始化
         if ( this.type === 'slide' ) {
@@ -220,24 +249,38 @@ define(['jquery'], function ($) {
         };
         /**
          * displayContentTab 展示页卡
-         * @param  {Number} index 展示的索引值
+         * @param  {Number}  index 展示的索引值
+         * @param  {Boolean} flag  来源 true-事件触发 / false-goIndex底层接口(滑动的时候有)
          */
-        var displayContentTab = function (index) {
+        var displayContentTab = function (index, flag) {
+            flag = flag || false;
             if ( _this.type === 'tab' ) {
                 // tab 页卡切换时的展示逻辑
-                displayContentTab = function (index) {
+                displayContentTab = function (index, flag) {
+                    // 切换前
+                    $.proxy(_this.switchBefore, _this.tabContentTags.get(_this.defShowTabIdx))(_this.defShowTabIdx);
+                    if (_this.defShowTabIdx - index === 0) {
+                        return;// 不做改变
+                    }
                     $.each(_this.tabContentTags, function (idx, ele) {
-                        $(ele).css('display', 'none');
                         if ( index === idx ) {
                             $(ele).css('display', 'block');
+                        } else {
+                            $(ele).css('display', 'none');
                         }
                     });
                     _this.defShowTabIdx = index;
+
+                    // 切换后
+                    $.proxy(_this.switchAfter, _this.tabContentTags.get(_this.defShowTabIdx))(_this.defShowTabIdx);
                 };
             } else if ( _this.type === 'slide' ) {
                 // 滑动页卡切换时的展示逻辑
-                displayContentTab = function (index) {
-                    var step = _this.step || _this.tabContentTags.eq(0).width();
+                displayContentTab = function (index, flag) {
+                    // 切换前
+                    $.proxy(_this.switchBefore, _this.tabContentTags.get(_this.defShowTabIdx))(_this.defShowTabIdx);
+
+                    var step = _this.step || _this.tabContentTags.eq(0).outerWidth(true);
                     var distance = -1 * (index + 1) * step;
                     var adjustPosition = function () {
                         _this.tabContentContainer.css({'marginLeft': distance});
@@ -247,19 +290,22 @@ define(['jquery'], function ($) {
                     if ( index - _this.defShowTabIdx === 0 ) {
                         _this.tabContentContainer.css('marginLeft', distance);// 初始化定位
                         return;// 不作展示渲染
-                    } else if ( index - _this.defShowTabIdx ===  1 - _this.tabSize ) {// 由尾切换到头
+                    } else if ( !flag && index - _this.defShowTabIdx ===  1 - _this.tabSize ) {// 由尾切换到头
                         _this.tabContentContainer.stop(true, true).animate({marginLeft: -1 * (_this.defShowTabIdx + 2) * step}, 300, function () { adjustPosition(index); });
                         return;
-                    } else if ( index - _this.defShowTabIdx === _this.tabSize - 1 ) {// 由头切换到尾
+                    } else if ( !flag && index - _this.defShowTabIdx === _this.tabSize - 1 ) {// 由头切换到尾
                         _this.tabContentContainer.stop(true, true).animate({marginLeft: _this.defShowTabIdx * step}, 300, function () { adjustPosition(index); });
                         return;
                     }
                     // 动画渲染主函数
                     _this.tabContentContainer.stop(true, true).animate({marginLeft: distance}, 300);
                     _this.defShowTabIdx = index;
+
+                    // 切换后
+                    $.proxy(_this.switchAfter, _this.tabContentTags.get(_this.defShowTabIdx))(_this.defShowTabIdx);
                 };
             }
-            return displayContentTab(index);
+            return displayContentTab(index, flag);
         };
         /**
          * goIndex 切换到制定的页卡
@@ -274,10 +320,18 @@ define(['jquery'], function ($) {
          * @param  {Number} idx 索引值
          * @return {Number}     经过边界校验的索引值
          */
-        var looping = function (idx) {
+        var looping = (function () {
             var max = _this.tabSize - 1;
-            return idx > max ? 0 : idx < 0 ? max : idx;
-        };
+            if ( _this.isLoop ) {// 循环
+                return function (idx) {
+                    return idx > max ? 0 : idx < 0 ? max : idx;
+                };
+            } else {// 不循环
+                return function (idx) {
+                    return idx > max ? max : idx < 0 ? 0 : idx;
+                };
+            }
+        })();
         /**
          * autoToggle 页卡自动切换
          */
@@ -307,43 +361,74 @@ define(['jquery'], function ($) {
         };
 
         /*************************************************************回调事件句柄&&事件绑定**************************************************************/
+
+        // 获取事件对象
+        var getEvent = function (e) {
+            return e || window.event;
+        };
+        // 阻止默认事件
+        var preventEvent = function (e) {
+            if (e.preventDefault) {
+                e.preventDefault();
+            } else {
+                e.returnValue = false;
+                return false;
+            }
+        };
+        // 取消冒泡
+        var cancelBubble = function (e) {
+            if (e.stopPropagation) {
+                e.stopPropagation();
+            } else {
+                e.cancelBubble = true;
+                return false;
+            }
+        };
+
         /**
          * tabToggle 切换事件回调
-         * @param  {Object} e jq封装的事件
+         * @param  {Object}  e      jq封装的事件
+         * @param  {DOM}     ele    DOM元素
          */
-        var tabToggle = function (e) {
+        var tabToggle = function (e, ele) {
             var index;
-                index = toggleClass(this);
-                displayContentTab(index);
+                index = toggleClass(ele);
+                displayContentTab(index, true);
         };
         /**
          * backward 回退事件回调
          */
         var backward = function () {
-            // _this.defShowTabIdx = looping(_this.defShowTabIdx - 1);
             goIndex(looping(_this.defShowTabIdx - 1));
         };
         /**
          * forward 前进事件回调
          */
         var forward = function () {
-            // _this.defShowTabIdx = looping(_this.defShowTabIdx + 1);
             goIndex(looping(_this.defShowTabIdx + 1));
         };
 
         // tab页签事件注册
-        this.tabContainer.on(this.eventType, this.tabTag, tabToggle);
+        this.tabContainer.on(this.eventType, this.tabTag, function (e) {
+            e = getEvent();
+            preventEvent(e);
+            tabToggle(e, this);
+        });
 
         // 加绑定条件限制
         if ( this.preBtnId && this.nextBtnId ) {
 
             // 左侧按钮
             $('#' + this.preBtnId).on('click', function (e) {
+                e = getEvent();
+                preventEvent(e);
                 backward();
             });
 
             // 右侧按钮
             $('#' + this.nextBtnId).on('click', function (e) {
+                e = getEvent();
+                preventEvent(e);
                 forward();
             });
 
@@ -371,6 +456,33 @@ define(['jquery'], function ($) {
         // 判断是否需要自动轮播
         if ( _this.isAuto && _this.timing ) {
             autoToggle();
+        }
+    };
+
+    /**
+     * unbindDOM 解绑
+     * @param  {String} type tab-解绑页签 | btn-解绑按钮 | 默认全部解绑
+     */
+    Focus.prototype.unbindDOM = function (type) {
+        var _this = this;
+
+        // 取消tab事件
+        var delTab = function () {
+            _this.tabContainer.off(_this.eventType);
+        };
+        // 取消btn事件
+        var delBtn = function () {
+            $('#' + _this.preBtnId).off('click');
+            $('#' + _this.nextBtnId).off('click');
+        };
+
+        if (type === 'tab') {
+            delTab();
+        } else if (type === 'btn') {
+            delBtn();
+        } else {
+            delTab();
+            delBtn();
         }
     };
 
