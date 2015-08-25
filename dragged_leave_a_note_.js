beef1d079cd3866a77515eac695cf6cc27a1905b
behindli <behindli@tencent.com>
2015-08-25 15:24:27 +0800

    添加mouseover与mouseout事件目标对象指向.

--- dragged_leave_a_note_.js
+++ dragged_leave_a_note_.js
@@ -1,3 +1,10 @@
 $(window).on('mousemove', 'body', function () {
     window.getSelection ? window.getSelection().removeAllRanges() : document.selection.empty();
 });
+
+/**
+ *
+ *  onmouseover         var reltg = e.relatedTarget ? e.relatedTarget : e.fromElement;
+ *  onmouseout          var reltg = e.relatedTarget ? e.relatedTarget : e.toElement;
+ *
+ */
