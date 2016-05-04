5e9c96d76523afdc103f001827c9e5d7215f2296
behindli <behindli@tencent.com>
2016-05-04 17:48:05 +0800

    增加mysql目录, 加入select语句用法, 备忘

---
+++ Mysql/select.sql
@@ -0,0 +1,31 @@
+SELECT
+    [ALL | DISTINCT | DISTINCTROW ]
+      [HIGH_PRIORITY]
+      [MAX_STATEMENT_TIME = N]
+      [STRAIGHT_JOIN]
+      [SQL_SMALL_RESULT] [SQL_BIG_RESULT] [SQL_BUFFER_RESULT]
+      [SQL_CACHE | SQL_NO_CACHE] [SQL_CALC_FOUND_ROWS]
+    select_expr [, select_expr ...]
+    [FROM table_references
+      [PARTITION partition_list]
+    [WHERE where_condition]
+    [GROUP BY {col_name | expr | position}
+      [ASC | DESC], ... [WITH ROLLUP]]
+    [HAVING where_condition]
+    [ORDER BY {col_name | expr | position}
+      [ASC | DESC], ...]
+    [LIMIT {[offset,] row_count | row_count OFFSET offset}]
+    # tips
+    # 第一个参数指定第一个返回记录行的偏移量
+    #   初始记录行的偏移量是 0(而不是 1)
+    # 第二个参数指定    返回记录行的最大数目
+    # demo
+    # mysql> SELECT * FROM table LIMIT 5,10;  // 检索记录行 6-15
+    # 换句话说，LIMIT n 等价于 LIMIT 0,n
+    [PROCEDURE procedure_name(argument_list)]
+    [INTO OUTFILE 'file_name'
+        [CHARACTER SET charset_name]
+        export_options
+      | INTO DUMPFILE 'file_name'
+      | INTO var_name [, var_name]]
+    [FOR UPDATE | LOCK IN SHARE MODE]]
