11619cc6758c9b9bca809cdc62dec6b5416b59cc
behindli <behindli@tencent.com>
2016-05-13 10:58:14 +0800

    加入报错信息，方便记忆

--- React/react_lifecycle_es6.js
+++ React/react_lifecycle_es6.js
@@ -17,4 +17,6 @@ class Counter extends Component {
  * getDefaultProps
  */
 // 如果是使用 ES6 语法，可直接定义 defaultProps 这个类属性来替代
+// 报错信息提示：
+// index.js:27359 Warning: getDefaultProps was defined on HotTag, a plain JavaScript class. This is only supported for classes created using React.createClass. Use a static property to define defaultProps instead.
 Counter.defaultProps = { initialCount: 0 };
