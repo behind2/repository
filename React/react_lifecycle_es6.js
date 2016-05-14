50e8bfce18fc82087465dec24238a482592d0243
behind2 <468113607@qq.com>
2016-05-14 14:42:11 +0800

    增加dom操作

--- React/react_lifecycle_es6.js
+++ React/react_lifecycle_es6.js
@@ -20,3 +20,17 @@ class Counter extends Component {
 // 报错信息提示：
 // index.js:27359 Warning: getDefaultProps was defined on HotTag, a plain JavaScript class. This is only supported for classes created using React.createClass. Use a static property to define defaultProps instead.
 Counter.defaultProps = { initialCount: 0 };
+
+
+/**
+ * findDOMNode()	不能用在无状态组件上
+ * 					当组件加载到页面上之后, 拿到对应的DOM元素
+ */
+componentDidMount() {
+  const el = findDOMNode(this);
+}
+
+/**
+ * Refs
+ * 这个属性的适用场景应该是：组件还没有渲染到页面上时需要操作dom的场景
+ */
