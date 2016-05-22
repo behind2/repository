36b18f5fc2bfec339cb684a98270c59bf04b6cf8
behindli <behindli@tencent.com>
2016-05-22 17:04:56 +0800

    增加update.sql

---
+++ Mysql/update.sql
@@ -0,0 +1,11 @@
+UPDATE [LOW_PRIORITY] [IGNORE] table_reference
+    SET col_name1={expr1|DEFAULT} [, col_name2={expr2|DEFAULT}] ...
+    [WHERE where_condition]
+    [ORDER BY ...]
+    [LIMIT row_count]
+
+
+
+UPDATE [LOW_PRIORITY] [IGNORE] table_references
+    SET col_name1={expr1|DEFAULT} [, col_name2={expr2|DEFAULT}] ...
+    [WHERE where_condition]
