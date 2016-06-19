5039ed9b347523c0dda2b07152aa60eb1d410159
behind2 <468113607@qq.com>
2016-06-19 20:10:50 +0800

    调整mysql连接顺序

--- Mysql/connect.sql
+++ Mysql/connect.sql
@@ -4,7 +4,7 @@
 -- character-set-server=utf8
 
 -- mysql登陆
-mysql -u <用户名> -h <服务器IP地址>  -P <端口号> -p<密码> -D <数据库名>
+mysql -u <用户名> -p<密码> -h <服务器IP地址> -P <端口号> -D <数据库名>
 
 -- 没写密码的话, 就是在client时候在输入
 
