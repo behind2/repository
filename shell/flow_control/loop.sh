1ea161fc41b4cf57280750354d5b175163276ee6
behind2 <468113607@qq.com>
2016-06-19 15:59:57 +0800

    for循环的第二种写法

--- shell/flow_control/loop.sh
+++ shell/flow_control/loop.sh
@@ -1,4 +1,5 @@
 # 循环语句
+# 写法一
 for var in item1 item2 ... itemN; do
 	#statements
 	command1;
@@ -7,3 +8,13 @@ for var in item1 item2 ... itemN; do
 	commandN;
 done
 
+
+
+# 写法二
+for (( i = 0; i < 10; i++ )); do
+	#statements
+	command1;
+	command2;
+	...
+	commandN;
+done
