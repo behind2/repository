eba50e2adb4774f95ec669ae22f89c38c3db6ca8
behind2 <468113607@qq.com>
2016-06-19 20:07:23 +0800

    增加mysql退出方法

--- Mysql/connect.sql
+++ Mysql/connect.sql
@@ -3,6 +3,14 @@
 -- default-character-set=utf8
 -- character-set-server=utf8
 
+-- mysql登陆
 mysql -u <用户名> -h <服务器IP地址>  -P <端口号> -p<密码> -D <数据库名>
 
 -- 没写密码的话, 就是在client时候在输入
+
+
+
+-- mysql退出连接
+exit;
+quit;
+\q;
