70e1acadd654f8e3ba0e0cc13c16988a3d66b435
behind2 <468113607@qq.com>
2016-06-20 22:49:53 +0800

    增加数据库查看指令, 查看告警指令

--- Mysql/database/create.sql
+++ Mysql/database/create.sql
@@ -3,6 +3,14 @@ create {database | schema} [if not exists] db_name
 [default] character set [=] charset_name
 
 
+-- 查看数据库列表
+show {databases | schemas}
+[like 'pattern' | where expr]
+
+-- 查看警告信息
+show warnings
+
+
 -- std
 
 CREATE {DATABASE | SCHEMA} [IF NOT EXISTS] db_name
