fe8e0b6b104b49dc8790f7e649b2b19817df052e
behind2 <468113607@qq.com>
2016-06-20 22:59:44 +0800

    增加 修改数据库指令

--- Mysql/database/create.sql
+++ Mysql/database/create.sql
@@ -19,3 +19,7 @@ show warnings
 
 -- 查看创建数据库时的相关设置
 show create {database | schema} [if not exists] db_name
+
+-- 修改数据库
+alter {database | schema} [db_name]
+[default] character set [=] charset_name
