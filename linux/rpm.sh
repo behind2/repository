84183664fedd2b776f5e63c0d10de416674d9c10
behind2 <468113607@qq.com>
2016-07-26 08:49:05 +0800

    增加rpm的其余命令

--- linux/rpm.sh
+++ linux/rpm.sh
@@ -5,3 +5,16 @@ rpm -ivh 包全名
 	-v (verbose) 显示详细信息
 	-h (hash) 显示进度
 	--nodeps 不检测依赖性
+
+
+# rpm 包升级
+rpm -Uvh 包全名
+-options
+	-U (upgrade) 升级
+
+
+# rpm 卸载
+rpm -e 包名
+-options
+	-e (erase) 卸载
+	--nodeps 不检测依赖性
