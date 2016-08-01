9d9263c4ebbde4d24c341912af1b6c69c61bd9c8
behind2 <468113607@qq.com>
2016-08-01 08:56:09 +0800

    主键不一定与auto_increment使用

--- Mysql/curd/table.sql
+++ Mysql/curd/table.sql
@@ -13,7 +13,8 @@ desc tbl_name
 auto_increment
 
 -- 主键(primary key)
-1.主键约束
-2.每张数据表只能存在一个主键
-3.主键保证记录的唯一性
-4.主键自动为 not null
+-- 1.主键约束
+-- 2.每张数据表只能存在一个主键
+-- 3.主键保证记录的唯一性
+-- 4.主键自动为 not null
+-- 5.主键不一定与auto_increment使用
