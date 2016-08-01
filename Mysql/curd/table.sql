0a413680ae40e74fb9f6e70bd7f4d3f415ce49c7
behind2 <468113607@qq.com>
2016-08-01 08:40:33 +0800

    增加auto_increment和primary key

--- Mysql/curd/table.sql
+++ Mysql/curd/table.sql
@@ -8,3 +8,12 @@ show columns from tbl_name
 -- 查看数据表结构
 desc tbl_name
 
+-- 自动编号 与主键组合使用
+-- 默认情况下, 起始值为1, 每次的增量为1
+auto_increment
+
+-- 主键(primary key)
+1.主键约束
+2.每张数据表只能存在一个主键
+3.主键保证记录的唯一性
+4.主键自动为 not null
