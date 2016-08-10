0e5e46b91c24aa8e74db6899c7d6637483f5b4d2
behind2 <468113607@qq.com>
2016-08-10 20:23:51 +0800

    新增兼容写法

--- components/getScript.js
+++ components/getScript.js
@@ -10,3 +10,36 @@
 		}
 		head.appendChild(js);
 	}
+
+
+
+	  static loadScript(url, callback) {
+    var head = document.getElementsByTagName('head')[0];
+    var script = document.createElement('script');
+        script.type = 'text/javascript';
+
+    if (script.readyState) {
+      script.onreadystatechange = function() {
+        if (this.readyState === 'loaded' || this.readyState === 'complete') {
+          this.onreadystatechange = null;
+          this.setAttribute('src', null); /*删除src属性, 防止内存泄露*/
+          head.removeChild(this);
+          if (callback && typeof callback === 'function') {
+            callback();
+          }
+        }
+      };
+    } else {
+      script.onload = function() {
+        this.onload = null;
+        this.setAttribute('src', null); /*删除src属性, 防止内存泄露*/
+        head.removeChild(this);
+        if (callback && typeof callback === 'function') {
+          callback();
+        }
+      };
+    }
+
+    script.setAttribute('src', url);
+    head.appendChild(script);
+  }
