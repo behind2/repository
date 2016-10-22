afaad4c73fc8e7f1cb1cf9d871d11baf40140c71
behind2 <468113607@qq.com>
2016-10-22 17:03:12 +0800

    增加nslookup, dig相关命令操作

--- linux/dns/dns.sh
+++ linux/dns/dns.sh
@@ -4,3 +4,23 @@
 host -t SOA baidu.com
 host -t NS baidu.com
 host -t A baidu.com
+
+# nslookup
+nslookup www.baidu.com
+# nslookup 交互模式
+nslookup
+server 127.0.0.1
+# soa 记录
+set q=soa
+baidu.com
+# a 记录
+set q=a
+baidu.com
+
+# dig 命令
+dig @name server domain
+dig @127.0.0.1 www.baidu.com
+# 反向解析
+dig -x ip @name server
+# a 记录
+dig -t a baidu.com
