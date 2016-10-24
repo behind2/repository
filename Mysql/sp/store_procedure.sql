e0d36a086c40f04046fbfc5a2eda4439291a0065
behind2 <468113607@qq.com>
2016-10-24 09:05:05 +0800

    添加相关注释

--- Mysql/sp/store_procedure.sql
+++ Mysql/sp/store_procedure.sql
@@ -21,11 +21,11 @@ type:
     Any valid MySQL data type
 
 characteristic:
-    COMMENT 'string'
+    COMMENT 'string' -- 注释
   | LANGUAGE SQL
   | [NOT] DETERMINISTIC
-  | { CONTAINS SQL | NO SQL | READS SQL DATA | MODIFIES SQL DATA }
-  | SQL SECURITY { DEFINER | INVOKER }
+  | { CONTAINS SQL | NO SQL | READS SQL DATA | MODIFIES SQL DATA } -- 包含sql语句，但不包含读或写数据的语句 | 不包含sql语句 | 包含读数据的语句 | 包含写数据的语句
+  | SQL SECURITY { DEFINER | INVOKER } -- 指明谁有权限来执行
 
 routine_body:
     Valid SQL routine statement
