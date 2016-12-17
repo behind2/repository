a6836488869184481ea9d3a0302160b7724eedef
behind2 <468113607@qq.com>
2016-12-17 21:46:54 +0800

    增加原型链的理解

--- React/proto.js
+++ React/proto.js
@@ -1,4 +1,6 @@
 /**
  * @quote https://github.com/creeperyang/blog/issues/9
  * 对象的__proto__指向自己构造函数的prototype
+ * 最后总结：先有Object.prototype（原型链顶端），Function.prototype继承Object.prototype而产生，
+ * 最后，Function和Object和其它构造函数继承Function.prototype而产生。
  */
