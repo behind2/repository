04cb39732c396614a81f015792624294cf082501
behind2 <468113607@qq.com>
2017-10-08 22:50:29 +0800

    TS初识

---
+++ TypeScript/Hello.js
@@ -0,0 +1,9 @@
+"use strict";
+exports.__esModule = true;
+var Hello = /** @class */ (function () {
+    function Hello() {
+    }
+    Hello.prototype.test = function () { };
+    return Hello;
+}());
+exports.Hello = Hello;
