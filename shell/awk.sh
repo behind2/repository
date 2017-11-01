757069e497211f673e4924189f50ed4d7004f7fb
behind2 <468113607@qq.com>
2017-11-01 09:05:34 +0800

    awk 内置参数

--- shell/awk.sh
+++ shell/awk.sh
@@ -3,10 +3,19 @@
 
 # 命令行格式
 # $ awk [options] 'command' files
-#                  command: pattern {awk 操作命令}
+#                  command: pattern {awk操作命令}
 #                           pattern: 正则表达式; 逻辑判断式
-#   awk 操作命令: 内置函数: print(), printf(), getline...
+#   awk操作命令: 内置函数: print(), printf(), getline...
 #                控制指令: if () {...} else {...}, while () {...}
+
+# awk内置参数应用
+# 内置变量
+#   $0: 表示整个当前行
+#   $1: 每行第一个字段
+#   $2: 每行第二个字段
+# 分隔符
+# options: -F field-separator(默认为空格)
+# ex: $ awk -F ':' '{print $3}' /etc/passwd
 
 # 脚本格式
 # $ awk -f awk-script-file files
